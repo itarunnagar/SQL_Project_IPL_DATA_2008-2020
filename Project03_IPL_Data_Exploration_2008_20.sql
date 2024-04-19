d9ea37ee82d941bb2be324ce9e347594b7a83f23
Tarun Nagar <itarunnagar@gmail.com>
2024-04-19 06:18:19 +0530

    Work Remainig

---
+++ Project03_IPL_Data_Exploration_2008_20.sql
@@ -0,0 +1,19 @@
+--USe the Database...
+use Project03_IPL_Data_Exploration_2008_20 ; 
+
+--See the DataBAse .....
+select * from dataset_01 ;
+select * from dataset_02 ;
+
+-- Count the Numbers of Rows
+select count(*)  as Numbers_of_ROws from Dataset_01 ; 
+select count(*) as Numbers_of_ROws from Dataset_02 ;
+
+--If there is Large Amount of Dataset Then we can break that dataset and make new tables and then decide the column
+-- and in the insert value we can  join all that table we are dividing so it will make our dataset same...
+
+--Problem or Question That Solvd..
+
+
+
+
